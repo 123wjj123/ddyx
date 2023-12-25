825043a04d4c1a0be42bce5b0bff243132d6157d
wjj <971010446@qq.com>
2023-12-25 11:23:37 +0800

    小组项目

--- spzx-common/common-log/src/main/java/com/atguigu/spzx/common/log/service/AsyncOperLogService.java
+++
@@ -1,8 +0,0 @@
-package com.atguigu.spzx.common.log.service;
-
-import com.atguigu.spzx.model.entity.system.SysOperLog;
-
-public interface AsyncOperLogService {
-
-    public abstract void saveSysOperLog(SysOperLog sysOperLog) ;
-}
--- spzx-service/service-pay/src/main/java/com/atguigu/spzx/pay/service/AlipayService.java
+++
@@ -1,7 +0,0 @@
-package com.atguigu.spzx.pay.service;
-
-public interface AlipayService {
-
-    //支付宝下单
-    String submitAlipay(String orderNo);
-}
